b16fa7b722f163f2741f33bd4d279a267dfe598c
Yijing Luo <yluo@datalogics.com>
2017-05-23 11:14:51 -0500

    Adding flattener worker
    Tested on Windows platform

--- Multi-Threading/MultiThreadingSample/MultiThreadingSample.cpp
+++ Multi-Threading/MultiThreadingSample/MultiThreadingSample.cpp
@@ -71,6 +71,7 @@ class PDFxWorker;
 class XPS2PDFWorker;
 class TextextWorker;
 class RasterizerWorker;
+class FlattenWorker;
 
 typedef union workerclassptr
 {
@@ -80,6 +81,7 @@ typedef union workerclassptr
     XPS2PDFWorker       *XPS2PDF;
     TextextWorker       *TextExtract;
     RasterizerWorker    *Rasterizer;
+    FlattenWorker       *Flattener;
 } WorkerClassPtr;
 
 
@@ -90,7 +92,8 @@ typedef enum
     PDFX,
     XPS2PDF,
     TextExtract,
-    Rasterizer
+    Rasterizer,
+    Flattener
 };
 
 typedef struct worktypes
@@ -109,7 +112,8 @@ WorkerType workers[] = {
     {"PDFx",        "PDFxOptions",           PDFX },
     {"XPS2PDF",     "XPS2PDFOptions",        XPS2PDF },
     {"TextExtract", "TextExtractOptions",    TextExtract },
-    { "Rasterizer", "RasterizerOptions",     Rasterizer }
+    {"Rasterizer", "RasterizerOptions",     Rasterizer },
+    {"Flattener", "FlattenOptions", Flattener }
 };
 #define NumberOfWorkers (sizeof (workers)/sizeof (WorkerType))
 
@@ -1145,9 +1149,6 @@ public:
 
     }
 
-
-
-
     void WorkerThread (ThreadInfo *info)
     {
         int sequence = info->sequence;
@@ -1249,6 +1250,174 @@ private:
     ASInt8      colorComponents;
 };
 
+/*
+** Flattener worker
+** This thread will flatten document.
+** NoAPDFL MUST be false, or the thread will not function.
+** InFileName as a list will use the Nth entry of the list, modulus the number of entries in the list, for each worker thread.
+
+**  FlattenOptions=[
+**                   silent=true                                        When true, do not display status lines
+**                   InFileName=[test data path\AddRedactions.pdf]      A list of file to copy, One file will be copied by each thread, sequcence % #files.
+**                   OutFilePath=[Output]                               Directory where output is written
+**                   noAPDFL=false                                      When true, we will NOT init/term the library for each thread
+*/
+#include "PDFlattenerCalls.h"
+#include <iomanip>
+ASBool flattenerProgMon(ASInt32 pageNum, ASInt32 totalPages, float current, ASInt32 reserved, void *clientData)
+{
+    //The previous page we were working on.
+    ASInt32* prevPage = (ASInt32*)clientData;
+
+    //If we've begun a new page, or if we've finished.
+    if (pageNum != (*prevPage) || current == 100.0f)
+    {
+        //Print the completion percentage.
+        std::cout << "[" << std::fixed << std::setw(6) << std::setfill('0') << std::setprecision(2) << current << "%] ";
+
+        //Print the current page.
+        std::cout << "Flattening page " << pageNum + 1 << " of " << totalPages << ". " << std::endl;
+
+        //Update previous page.
+        *prevPage = pageNum;
+    }
+
+    //Return 1 to cancel Flattening.
+    return 0;
+}
+
+class FlattenWorker : public workerclass
+{
+#define NumberOfPDFAConvertOptions 4
+
+public:
+    FlattenWorker() {
+    };
+
+    ~FlattenWorker()
+    { };
+
+    /* Parse the PDF/a conversion thread options into attributes */
+    void ParseOptions(valuelist *values)
+    {
+        workerclass::ParseOptions(values, "%AddRedaction.pdf", "Output");
+    };
+
+    /* One thread worker procedure */
+    void WorkerThread(ThreadInfo *info)
+    {
+        int sequence = info->sequence;
+        if (!silent)
+            fprintf(info->logFile, "PDF/a Worker Thread Started! (Sequence: %01d, Thread: %01d\n", sequence + 1, info->threadNumber + 1);
+
+        /* Generate input and output file names */
+        char *fullFileName = GetInFileName(sequence);
+        char *fullOutputFileName = GetOutFileName(sequence, -1);
+
+        DURING
+        /* Open the input document */
+        APDFLDoc inDoc(fullFileName, true);
+
+        /* Free the input file names */
+        free(fullFileName);
+
+        /* Initialize the HFT for the plugin */
+        gPDFlattenerHFT = InitPDFlattenerHFT;;
+
+        //initialize PDFProcessor plugin
+        if (!PDFlattenerInitialize())
+            /* If the plugin cannot be initilized! */
+            info->result = 1;
+        else
+        {
+            /* COnstruct the user params recor for the flattening */
+            PDFlattenerUserParamsRec flattenParams;
+
+            memset(&flattenParams, 0, sizeof(PDFlattenerUserParamsRec));
+            flattenParams.size = sizeof(PDFlattenerUserParamsRec);
+            //A profiled color space to use for transparent objects. For CMYK, use "U.S. Web Coated (SWOP)v2".
+            flattenParams.profileDesc = ASTextFromUnicode((ASUTF16Val*)"sRGB IEC61966-2.1", kUTF8);
+            //The ZIP compression scheme (Flate encoding) for images.
+            flattenParams.colorCompression = kPDFlattenerZipCompression;
+            //Raster/Vector balance. Use 0.00f for no vectors.
+            flattenParams.transQuality = 100.0f;
+            // Callback options.
+
+            ASInt32 currentPage = -1;
+            //Progress monitor callback data. I'm using this data to store the previous page 
+            //   the Flattener was working on, using -1 as "hasn't begun yet".
+            flattenParams.progressClientData = (void*)&currentPage;
+            //The progress monitor callback function.
+            flattenParams.flattenProgress = flattenerProgMon;
+            // Tile flattening options
+
+            PDFlattenRec flattener;
+            memset(&flattener, 0, sizeof(PDFlattenRec));
+            flattener.size = sizeof(PDFlattenRec);
+
+            flattener.tilingMode = kPDNoTiling;
+            flattener.tileSizePts = 0;
+
+            //Resolution for flattening the interior of an atomic region.
+            flattener.internalDPI = 800.0f;
+            //Resolution for flattening edges of atomic regions.
+            flattener.externalDPI = 200.0f;
+
+            flattener.clipComplexRegions = false;
+            //If we convert stroked elements to filled elements.
+            flattener.strokeToFill = true;
+            //If we use rastered text instead of native text.
+            flattener.useTextOutlines = false;
+            //If we attempt to preserve overprint
+            flattener.preserveOverprint = true;
+
+            flattener.allowShadingOutput = true;
+            flattener.allowLevel3ShadingOutput = true;
+
+            //Maximum image size while flattening. 0 is default.
+            flattener.maxFltnrImageSize = 0;
+            //Adaptive flattening threshold. Doesn't matter, since we're not doing adaptive tiling. See tilingMode.
+            flattener.adaptiveThreshold = 0;
+
+            flattenParams.flattenParams = &flattener;
+
+            /* Perform the flatten document */
+            ASUns32 numFlattened = 0;
+            ASInt32 flattenResult = PDFlattenerConvertEx2(inDoc.getPDDoc(),      //The document whose pages we wish to flatten.
+                                                          0,                     //The first page to flatten.
+                                                          PDDocGetNumPages(inDoc.getPDDoc()) - 1,   //The last page to flatten.
+                                                          &numFlattened,         //PDFlattener sets this to the number of pages
+                                                          // it flattened. It will not flatten pages that do not contain transparent elements.
+                                                          &flattenParams);             //Flattener options.
+            if (flattenResult)
+            {
+                std::cout << "Flattened " << numFlattened << " pages." << std::endl;
+            }
+            else
+            {
+                std::cout << "Flattening failed." << std::endl;
+                ASRaise(GenError(genErrGeneral));
+            }
+            printf("outputfile name: %s\n", fullOutputFileName);
+            /* Save the output PDF Document */
+            inDoc.saveDoc(fullOutputFileName); //fullOutputFileName
+            /* Release the output file name */
+            free(fullOutputFileName);
+            ASTextDestroy(flattenParams.profileDesc);
+            /* terminate the plugin */
+            PDFlattenerTerminate();
+        }
+        HANDLER
+            info->result = 2;
+        END_HANDLER
+
+            if (!silent)
+                fprintf(info->logFile, "PDF/a Worker Thread Completed! (Sequence: %01d, Thread: %01d\n", sequence + 1, info->threadNumber + 1);
+    }
+
+};
+
+
 /* This procedure calls a worker thread of a specific type, 
 ** based on the info->objectType field. 
 **
@@ -1279,6 +1448,9 @@ void callWorker (ThreadInfo *info)
         break;
     case Rasterizer:
         ((RasterizerWorker *)baseObject)->WorkerThread (info);
+        break;
+    case Flattener:
+        ((FlattenWorker *)baseObject)->WorkerThread(info);
         break;
     default:
         baseObject->WorkerThread (info);
@@ -1436,6 +1608,8 @@ int main(int argc, char** argv)
     workerClasses[Rasterizer].Rasterizer = new RasterizerWorker ();
     workerClasses[Rasterizer].Rasterizer->ParseOptions (SampleAttributes.GetKeyValue (workers[Rasterizer].paramName));
 
+    workerClasses[Flattener].Flattener = new FlattenWorker();
+    workerClasses[Flattener].Flattener->ParseOptions(SampleAttributes.GetKeyValue(workers[Flattener].paramName));
 
     /* This will be the list of threads to run */
     ThreadInfo *threads = (ThreadInfo *)malloc (sizeof (ThreadInfo) * totalThreads);
